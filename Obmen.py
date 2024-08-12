901db7bbc9ec9c4e395c25d26cd61718deef05d3
Я робот <fruktec7@gmail.com>
2024-08-12 22:24:04 +0300

    В метку добавили название валют

--- Obmen.py
+++ Obmen.py
@@ -3,6 +3,13 @@ from tkinter import  ttk
 from tkinter import messagebox as mb
 import requests
 import json
+
+def update_currency_label(event):
+    # Получаем полное название валюты из словаря и обновляем метку
+    code = combobox.get()
+    name = currencies[code]
+    currency_label.config(text=name)
+
 def exchange():
     code = combobox.get()
 
@@ -14,25 +21,38 @@ def exchange():
 
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-
-                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.2f} {code} за 1 доллар")
+                currency_name=currencies[code]
+                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.2f} {currency_name} за 1 доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")
         except Exception as e:
             mb.showerror("Ошибка", f"Ошибка: {e}")
-
     else:
-        mb.showwarning("Внимание", "Введите код валюты")
-
+        mb.showwarning("Внимание", "Выберите код валюты")
+currencies = {
+    "EUR": "Евро",
+    "JPY": "Японская йена",
+    "GBP": "Британский фунт стерлингов",
+    "AUD": "Австралийский доллар",
+    "CAD": "Канадский доллар",
+    "CHF": "Швейцарский франк",
+    "CNY": "Китайский юань",
+    "RUB": "Российский рубль",
+    "KZT": "Казахстанский тенге",
+    "UZS": "Узбекский сум"}
 
 window = Tk()
 window.title("Курс обмена валюты к доллару")
 window.geometry("360x180")
 
 Label(text="Выберите код валюты:").pack(padx=10, pady=10)
-cur=["RUB","EUR","GBP","JPY","CNY","KZT","CHF","AED","CAD"]
-combobox=ttk.Combobox(values=cur)
+
+combobox=ttk.Combobox(values=list (currencies.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_currency_label)
+
+currency_label = ttk.Label()
+currency_label.pack(padx=10, pady=10)
 
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
