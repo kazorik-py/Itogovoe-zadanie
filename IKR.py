c9aa212604bd3773a21e1df17472a0b922eea64f
Я робот <fruktec7@gmail.com>
2024-08-14 01:57:07 +0300

    Добавила русские названия валют, но они не выводятся. ??

--- IKR.py
+++ IKR.py
@@ -4,6 +4,13 @@ from tkinter import ttk
 import requests
 import json
 
+
+def update_c_label(event):
+    code = combobox.get()
+    #name = cur(code)   На что он ругается?
+    c_label.config(text=name)
+
+
 def exchange():
     code = combobox.get()
     if code:
@@ -23,16 +30,29 @@ def exchange():
         mb.showwarning("Внимание", "Введите код криптовалюты")
 
 
+cur = {
+    'rub':'Российский рубль',
+    'eur':'Евро',
+    'usd':'Американский доллар',
+    'jpy':'Японская йена',
+    'clp':'Чилийский пессо',
+    'dkk':'Датская крона',
+    'cny':'Китайский юань',
+    'aed':'Дирхам ОАЭ'}
+
 window = Tk()
 window.title("Курс криптовалют")
 window.geometry("400x200")
 
 Label(text="Выберите код криптовалюты").pack(padx=10, pady=10)
 
-cur = ['rub','eur','usd','jpy','clp','dkk','cny','aed']
-combobox = ttk.Combobox(values=cur)
-combobox.pack(padx=10, pady=10)
 
+combobox = ttk.Combobox(values=list(cur.keys()))
+combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_c_label)
+
+c_label = ttk.Label()
+c_label.pack(padx=10,pady=10)
 
 Button(text="Получить курс обмена к БИТКОИНУ", command=exchange).pack(padx=10, pady=10)
 
