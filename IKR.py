1d04a175c6e1d06f44adf6155c1c192df1ce9ed2
Я робот <fruktec7@gmail.com>
2024-08-14 00:18:44 +0300

    Прописаны переменные относительно нового  апи, удален энтри, добавлен комбобокc, создан список валют

--- IKR.py
+++ IKR.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 import requests
 import json
 
 def exchange():
-    code = entry.get().lower()  # Приводим введенный код к нижнему регистру
+    code = combobox.get()
     if code:
         try:
             response = requests.get("https://api.coingecko.com/api/v3/exchange_rates")
@@ -12,8 +13,8 @@ def exchange():
             data = response.json()
 
             if code in data['rates']:
-                exchange_rate = data['rates'][code]['value']
-                mb.showinfo("Курс обмена", f"Курс {code.upper()}: {exchange_rate}")
+                exchange_rates = data['rates'][code]['value']
+                mb.showinfo("Курс обмена", f"Курс {exchange_rates}: {code} за 1 биткоин???")
             else:
                 mb.showwarning("Внимание", "Код криптовалюты не найден")
         except Exception as e:
@@ -21,14 +22,17 @@ def exchange():
     else:
         mb.showwarning("Внимание", "Введите код криптовалюты")
 
+
 window = Tk()
 window.title("Курс криптовалют")
 window.geometry("400x200")
 
 Label(text="Выберите код криптовалюты").pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+cur = ['rub','eur','usd','jpy','clp','dkk','cny','aed']
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
+
 
 Button(text="Получить курс обмена к БИТКОИНУ", command=exchange).pack(padx=10, pady=10)
 
